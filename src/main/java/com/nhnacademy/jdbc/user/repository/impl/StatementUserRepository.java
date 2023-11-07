3db3a8fa3a1fec64e3afe1d8554d0a1b4af082a5
nhn-academy-marco <marco@ nhnacademy.com>
2023-11-07 10:10:12 +0900

    예제 코드 수정

--- src/main/java/com/nhnacademy/jdbc/user/repository/impl/StatementUserRepository.java
+++ src/main/java/com/nhnacademy/jdbc/user/repository/impl/StatementUserRepository.java
@@ -17,40 +17,13 @@ public class StatementUserRepository implements UserRepository {
     @Override
     public Optional<User> findByUserIdAndUserPassword(String userId, String userPassword) {
         //todo#1 아이디, 비밀번호가 일치하는 User 조회
-        String sql = String.format("select user_id, user_name, user_password from jdbc_users where user_id='%s' and user_password='%s' ", userId, userPassword);
-        log.debug("sql:{}",sql);
-        try(Connection connection = DbUtils.getConnection();
-            Statement statement = connection.createStatement();
-            ResultSet rs = statement.executeQuery(sql);
-        ) {
-            if(rs.next()){
-                return Optional.of(
-                        new User(rs.getString("user_id"),rs.getString("user_name"),rs.getString("user_password"))
-                );
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+
         return Optional.empty();
     }
 
     @Override
     public Optional<User> findById(String userId) {
         //#todo#2-아이디로 User 조회
-        String sql = String.format("select user_id, user_name, user_password from jdbc_users where user_id='%s'", userId);
-
-        try(Connection connection = DbUtils.getConnection();
-            Statement statement = connection.createStatement();
-            ResultSet rs = statement.executeQuery(sql);
-        ) {
-            if(rs.next()){
-                return Optional.of(
-                        new User(rs.getString("user_id"),rs.getString("user_name"),rs.getString("user_password"))
-                );
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
 
         return Optional.empty();
     }
@@ -58,55 +31,19 @@ public class StatementUserRepository implements UserRepository {
     @Override
     public int save(User user) {
         //todo#3- User 저장
-        String sql = String.format("insert into jdbc_users (user_id, user_name, user_password) values ('%s','%s','%s')",
-                user.getUserId(),
-                user.getUserName(),
-                user.getUserPassword()
-        );
-
-        try(Connection connection = DbUtils.getConnection();
-            Statement statement = connection.createStatement();
-        ){
-            int result = statement.executeUpdate(sql);
-            log.debug("save-result:{}", result);
-            return result;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        return 0;
     }
 
     @Override
     public int updateUserPasswordByUserId(String userId, String userPassword) {
         //todo#4-User 비밀번호 변경
-        String sql = String.format("update jdbc_users set user_password='%s' where user_id='%s'",
-                    userPassword,
-                    userId
-                );
-        log.debug("sql:{}",sql);
-        try(Connection connection = DbUtils.getConnection();
-            Statement statement = connection.createStatement();
-        ){
-            int result = statement.executeUpdate(sql);
-            log.debug("updateUserPasswordByUserId : {}", result);
-            return result;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        return 0;
     }
 
     @Override
     public int deleteByUserId(String userId) {
         //todo#5 - User 삭제
-        String sql = String.format("delete from jdbc_users where user_id='%s'", userId);
-        try(Connection connection = DbUtils.getConnection();
-            Statement statement = connection.createStatement();
-        ){
-            int result = statement.executeUpdate(sql);
-            log.debug("result:{}", result);
-            return result;
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+        return 0;
     }
 
 }
