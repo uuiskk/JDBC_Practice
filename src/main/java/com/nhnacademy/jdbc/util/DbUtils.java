5bc9599e5563a30f1fc71b85d17102976028705f
nhn-academy-marco <marco@ nhnacademy.com>
2023-11-07 13:26:57 +0900

    DbUtil mysql 접속정보 초기화

--- src/main/java/com/nhnacademy/jdbc/util/DbUtils.java
+++ src/main/java/com/nhnacademy/jdbc/util/DbUtils.java
@@ -1,11 +1,7 @@
 package com.nhnacademy.jdbc.util;
 
 import org.apache.commons.dbcp2.BasicDataSource;
-
 import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.time.Duration;
 
 public class DbUtils {
@@ -19,9 +15,9 @@ public class DbUtils {
         BasicDataSource basicDataSource = new BasicDataSource();
 
         //todo#0 {ip},{database},{username},{password}
-        basicDataSource.setUrl("jdbc:mysql://133.186.241.167:3306/nhn_academy_0");
-        basicDataSource.setUsername("nhn_academy_0");
-        basicDataSource.setPassword("1j0OTaJgkS1@$dWl");
+        basicDataSource.setUrl("jdbc:mysql://{ip}:3306/{database}");
+        basicDataSource.setUsername("{username}");
+        basicDataSource.setPassword("{password}");
 
         basicDataSource.setInitialSize(5);
         basicDataSource.setMaxTotal(5);
