b8082c624a8644e60e351e0ba3d3d45e599e0867
uuiskk <hus101405@naver.com>
2024-10-25 16:36:13 +0900

    실습

--- src/main/java/com/nhnacademy/jdbc/util/DbUtils.java
+++ src/main/java/com/nhnacademy/jdbc/util/DbUtils.java
@@ -14,9 +14,9 @@ public class DbUtils {
     static {
         BasicDataSource basicDataSource = new BasicDataSource();
         //todo#0 {ip},{database},{username},{password} 설정 합니다.
-        basicDataSource.setUrl("jdbc:mysql://{ip}:3306/{database}");
-        basicDataSource.setUsername("{username}");
-        basicDataSource.setPassword("{password}");
+        basicDataSource.setUrl("jdbc:mysql://133.186.241.167:3306/nhn_academy_50");
+        basicDataSource.setUsername("nhn_academy_50");
+        basicDataSource.setPassword("/B7owD_6TtCRJl6C");
 
         basicDataSource.setInitialSize(5);
         basicDataSource.setMaxTotal(5);
